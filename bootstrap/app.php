1ac6d63aa95d57d742709b45911bb3f02cc7c476
raniel-roque <raniel.roque0509@gmail.com>
2026-03-14 17:18:05 +0800

    Api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'device/notify',
+            'device/door-status',
+            'device/motion',
+            'app/door-status',
+            'app/response',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
